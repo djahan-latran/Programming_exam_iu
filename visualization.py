7b96b3ed9e943a1c0fc3a97c729192594d2261b9
djahan_latran <djahan.latran@gmail.com>
2024-03-25 11:29:12 +0100

    implemented and adjusted exception handling

--- visualization.py
+++ visualization.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as dp
-
+from exceptions import InvalidDataError, DataframeTypeError
+import pandas as pd
 class Graphics:
 
     def plot_train_data(self,data):
 
         try:
+            if not isinstance(data.train_data, pd.DataFrame):
+                raise DataframeTypeError
+
+            if data.train_data.columns[0] != "x":
+                raise InvalidDataError("Missing column 'x' at index 0.")
+
+            for column_name in data.train_data.columns:
+                for value in data.train_data[column_name]:
+                    if not isinstance(value,float):
+                        raise InvalidDataError
+
             for column in data.train_data.columns[1:]:
-                plt.plot(data.train_data["x"],data.train_data[column],label=column)
+                plt.plot(data.train_data.iloc[:,0],data.train_data[column],label=column)
+
 
             plt.title("Train functions")
             plt.legend()
@@ -17,14 +28,33 @@ class Graphics:
 
             plt.show()
 
-        except AttributeError as e:
-            print("Error: Data is missing the dataframe-attribute. Please load data first.", e)
-        except KeyError as e:
-            print("Error: A dataframe column is missing. Make sure the csv-file has "
-                  "a 'x'-coordinate column", e)
+        except DataframeTypeError as e:
+            print(f"Error: {e.message}")
 
+        except AttributeError as e:
+            print(f"Error: Data might be missing the 'dataframe' attribute. Please load data first. {e}")
+
+        except InvalidDataError as e:
+            print(f"Error: {e.message}")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
     def _plot_bestfit_helper(self,data):
+
+        if not isinstance(data.train_data, pd.DataFrame):
+            raise DataframeTypeError
+
+        if not isinstance(data.ideal_data, pd.DataFrame):
+            raise DataframeTypeError
+
+        if data.train_data.columns[0] != "x":
+            raise InvalidDataError("Missing column 'x' at index 0 in train dataframe.")
+
+        for column_name in data.train_data.columns:
+            for value in data.train_data[column_name]:
+                if not isinstance(value, float):
+                    raise InvalidDataError
 
         for train_idx, ideal_idx in data.bestfit_dict.items():
             func_name = data.ideal_data.columns[int(ideal_idx)]
@@ -43,15 +73,37 @@ class Graphics:
 
             plt.show()
 
+        except DataframeTypeError as e:
+            print(f"Error: {e.message}")
+
+        except InvalidDataError as e:
+            print(f"Error: {e.message}")
+
         except AttributeError as e:
-            print("Error: Data is missing an attribute."
-                  "Could be self.bestfit_dict. "
-                  "Please make sure that data is loaded " 
-                  "and the find_bestfit method has been used.", e)
+            print(f"Error: {e} "
+                  "Please make sure that train and ideal data are loaded " 
+                  "and the find_bestfit method has been used.")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
     def plot_test_data(self,data):
 
         try:
+            if not isinstance(data.test_data, pd.DataFrame):
+                raise DataframeTypeError
+
+            if data.test_data.columns[0] != "x":
+                raise InvalidDataError("Missing column 'x' at index 0.")
+
+            if data.test_data.columns[1] != "y":
+                raise InvalidDataError("Missing column 'y' at index 1.")
+
+            for column_name in data.test_data.columns:
+                for value in data.test_data[column_name]:
+                    if not isinstance(value, float):
+                        raise InvalidDataError
+
             plt.plot(data.test_data["x"], data.test_data["y"], "bo")
 
             plt.axvline(color= "black", linestyle= "--")
@@ -60,12 +112,18 @@ class Graphics:
 
             plt.show()
 
+        except DataframeTypeError as e:
+            print(f"Error: {e.message}")
+
+        except InvalidDataError as e:
+            print(f"Error: {e.message}")
+
         except AttributeError as e:
             print("Error: Data is missing an attribute. " 
-                  "Please make sure that data is loaded ", e)
-        except KeyError as e:
-            print("Error: A dataframe column is missing. "
-                  "Make sure the dataframe has a 'x'- and 'y'-coordinate column", e)
+                  f"Please make sure that data is loaded. {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
 
     def plot_filtered_test_data(self,data):
@@ -74,19 +132,28 @@ class Graphics:
             self._plot_bestfit_helper(data)
 
             plt.plot([point[0] for point in data.filtered_pts],
-                     [point[1] for point in data.filtered_pts], "bo", label="Testpoints")
+                     [point[1] for point in data.filtered_pts], "bo", label="Test points")
 
             plt.axvline(color= "black", linestyle= "--")
             plt.axhline(color= "black", linestyle= "--")
             plt.ylim(-20,None)
 
-            plt.title("Best-fitting ideal functions and filtered testpoints")
+            plt.title("Best-fitting ideal functions and filtered test points")
             plt.legend()
 
             plt.show()
 
+        except DataframeTypeError as e:
+            print(f"Error: {e.message}")
+
+        except InvalidDataError as e:
+            print(f"Error: {e.message}")
+
         except AttributeError as e:
-            print("Error: Data is missing an attribute. " 
-                  "Please make sure that data is loaded and " 
-                  "the find_bestfit and test_point_validation "
-                  "methods have both been used. ", e)
+            print(f"Error: Data is missing an attribute. " 
+                  f"Please make sure that data is loaded and " 
+                  f"the find_bestfit and test_point_validation "
+                  f"methods have both been used. {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
