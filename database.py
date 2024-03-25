2f488dfcd38e2931e7aaa09e73b7322b6bbefab2
djahan_latran <djahan.latran@gmail.com>
2024-03-25 11:25:25 +0100

    implemented more exceptions

--- database.py
+++ database.py
@@ -1,28 +1,47 @@
 import sqlite3
 from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import OperationalError, ArgumentError
 
 
 class DatabaseManager:
 
     def __init__(self,database_name):
+        try:
+            self.engine = create_engine(f"sqlite:///{database_name}.db")
+            print(f"Successfully initialized engine for database '{database_name}' ")
 
-        self.engine = create_engine(f"sqlite:///{database_name}.db")
+        except ArgumentError as e:
+            print(f"Error: {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
     def create_train_table_from_df(self,data):
         try:
             data.train_data.to_sql("Train data", self.engine, index= False, if_exists= "replace")
+
         except OperationalError as e:
             print(f"Error while trying to create table: {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
     def create_ideal_table_from_df(self,data):
         try:
             data.ideal_data.to_sql("Ideal data", self.engine, index=False, if_exists="replace")
+
         except OperationalError as e:
             print(f"Error while trying to create table: {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
     def create_validation_table_from_df(self,data):
         try:
             data.df_filtered_pts.to_sql("Test point validation", self.engine, index=False, if_exists="replace")
+
         except OperationalError as e:
             print(f"Error while trying to create table: {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
