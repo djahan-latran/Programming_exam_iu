761bdda5b67ad9ece47fd2f4cc9ac40514f16a73
djahan_latran <djahan.latran@gmail.com>
2024-03-25 11:28:19 +0100

    implemented unittesting for file loader

---
+++ tests/test_file_loader.py
@@ -0,0 +1,37 @@
+import unittest
+from file_loader import FileLoader
+
+
+class TestFileLoader(unittest.TestCase):
+
+    def test_load_and_construct_csv_attribute(self):
+
+        foldername = None
+        filename = None
+        fileloader = FileLoader(foldername=foldername, filename=filename)
+
+        with self.assertRaises(AttributeError):
+            fileloader.load_and_construct_csv()
+
+    def test_load_and_construct_csv_file_format(self):
+
+        foldername = "datensaetze"
+        filename = "unittest_fileloader.txt"
+        fileloader = FileLoader(foldername=foldername, filename=filename)
+
+        with self.assertRaises(ValueError):
+            fileloader.load_and_construct_csv()
+
+    def test_load_and_construct_csv_file(self):
+
+        foldername = "datensaetze"
+        filename = "train.csv"
+
+        fileloader = FileLoader(foldername=foldername, filename=filename)
+        file = fileloader.load_and_construct_csv()
+
+        self.assertIsNotNone(file)
+
+
+if __name__ == '__main__':
+    unittest.main()
