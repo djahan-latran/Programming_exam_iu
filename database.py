9c98d9b3ed069decf24dc268b9e47b612d68d251
djahan_latran <djahan.latran@gmail.com>
2024-04-04 11:59:17 +0200

    code clean-up

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
-import sqlite3
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError, ArgumentError
 
 
 class DatabaseManager:
 
-    def __init__(self,database_name):
+    def __init__(self, database_name):
         try:
             self.engine = create_engine(f"sqlite:///{database_name}.db")
             print(f"Successfully initialized engine for database '{database_name}' ")
@@ -16,9 +15,9 @@ class DatabaseManager:
         except Exception as e:
             print(f"Error: {e}")
 
-    def create_train_table_from_df(self,data):
+    def create_train_table_from_df(self, data):
         try:
-            data.train_data.to_sql("Train data", self.engine, index= False, if_exists= "replace")
+            data.train_data.to_sql("Train data", self.engine, index=False, if_exists="replace")
 
         except OperationalError as e:
             print(f"Error while trying to create table: {e}")
@@ -26,7 +25,10 @@ class DatabaseManager:
         except Exception as e:
             print(f"Error: {e}")
 
-    def create_ideal_table_from_df(self,data):
+        else:
+            print("'Train' table successfully created.")
+
+    def create_ideal_table_from_df(self, data):
         try:
             data.ideal_data.to_sql("Ideal data", self.engine, index=False, if_exists="replace")
 
@@ -36,7 +38,10 @@ class DatabaseManager:
         except Exception as e:
             print(f"Error: {e}")
 
-    def create_validation_table_from_df(self,data):
+        else:
+            print("'Ideal' table successfully created.")
+
+    def create_validation_table_from_df(self, data):
         try:
             data.df_filtered_pts.to_sql("Test point validation", self.engine, index=False, if_exists="replace")
 
@@ -45,3 +50,6 @@ class DatabaseManager:
 
         except Exception as e:
             print(f"Error: {e}")
+
+        else:
+            print("'Test point validation' table successfully created.")
